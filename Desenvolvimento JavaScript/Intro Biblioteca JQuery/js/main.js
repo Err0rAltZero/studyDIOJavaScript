2eb9bca5083fbc1dfd0859876e8d3299fae92aa1
Err0rAltZero <gceni10@gmail.com>
2021-02-25 17:21:52 -0300

    Finalizado

--- Desenvolvimento JavaScript/Intro Biblioteca JQuery/js/main.js
+++ Desenvolvimento JavaScript/Intro Biblioteca JQuery/js/main.js
@@ -1,4 +1,5 @@
 function consultaCep(){
+    $(".barra-progresso").show();
     var cep = document.getElementById("cep").value;
     var url = "http://viacep.com.br/ws/"+cep+"/json/"
     console.log(url);
@@ -7,9 +8,20 @@ function consultaCep(){
         type: "GET",
         success: function(response){
             console.log(response);
+            $("#logradouro").html(response.logradouro);
+            $("#uf").html(response.uf);
+            $("#localidade").html(response.localidade);
+            $("#bairro").html(response.bairro);
+            $("#titulo_cep").html("CEP " + response.cep);
+            $(".cep").show();
+            $(".barra-progresso").hide();
             //$("#logradouro").html(response.logradouro); //Pega pelo ID: #
             //document.getElementById("logradouro").innerHTML = response.logradouro;
             //alert(response.logradouro);
         }
     })//JQuery <<
 }
+$(function(){
+    $(".cep").hide();
+    $(".barra-progresso").hide();
+});
